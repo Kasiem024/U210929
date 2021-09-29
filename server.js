8606b876e75cb94d8628a4e1f7cae50186a8c9ce
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-29 13:39:03 +0200

    heroku should work now

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-'use strict';
+'use strict'
 
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = process.env.port || 8042;
+const PORT = process.env.PORT || 8042;
 
 
-app.get('/app.html', (req, res) => {
+app.get('/', (req, res) => {
     fs.readFile('app.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
         res.setHeader('Content-Length', data.length);
         res.send(data);
     });
 })
+
 app.get('/exercise2.js', (req, res) => {
     fs.readFile('exercise2.js', (err, data) => {
+        res.setHeader('Content-Type', 'text/javasript');
+        res.setHeader('Content-Length', data.length);
         res.send(data);
     });
 })
 
-app.listen(port, () => {
-    console.log(`Server up and listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server up and listening on PORT ${PORT}`);
 })
