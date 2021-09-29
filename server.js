53b167d909f9965afa11e9747d5a71a280f19135
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-29 12:32:58 +0200

    works with express now changed name to app.html

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 8042;
 
-app.get('/', (req, res) => {
-    res.send('HELLO WORLD!');
+app.get('/app.html', (req, res) => {
+    fs.readFile('app.html', (err, data) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Length', data.length);
+        res.send(data);
+    });
 })
 
 app.listen(port, () => {
-    console.log(`Server up and listening on port ${PORT}`);
+    console.log(`Server up and listening on port ${port}`);
 })
