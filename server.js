a4bb319d701f9caa90d631afeb1b9544f717cfb4
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-29 12:09:06 +0200

    changed port to 8042

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const app = express();
+const port = 8042;
 
 app.get('/', (req, res) => {
-    res.send('HELLO WORLD!')
+    res.send('HELLO WORLD!');
 })
 
-app.listen(port, () =>{
-    console.log('Server up and listening on port ${PORT}');
+app.listen(port, () => {
+    console.log(`Server up and listening on port ${PORT}`);
 })
