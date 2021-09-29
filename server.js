78456bc05fc5e3b8fe4e43385faeb2f647efced0
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-29 13:52:30 +0200

    using oscars server

--- server.js
+++ server.js
@@ -2,23 +2,27 @@
 
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 8042;
 
-
 app.get('/', (req, res) => {
+
     fs.readFile('app.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
         res.send(data);
-    });
+  });
+
 });
 
 app.get('/exercise2.js', (req, res) => {
+
     fs.readFile('exercise2.js', (err, data) => {
         res.send(data);
-    });
+  });
+
 });
 
 app.listen(PORT, () => {
-    console.log(`Server up and listening on PORT ${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
