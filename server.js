24f151d80f401b7ffcc956bbd3ad73d4a82c93a7
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-09-29 13:50:56 +0200

    removed lines

--- server.js
+++ server.js
@@ -9,15 +9,12 @@ const PORT = process.env.PORT || 8042;
 app.get('/', (req, res) => {
     fs.readFile('app.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
-        res.setHeader('Content-Length', data.length);
         res.send(data);
     });
 });
 
 app.get('/exercise2.js', (req, res) => {
     fs.readFile('exercise2.js', (err, data) => {
-        res.setHeader('Content-Type', 'text/javascript');
-        res.setHeader('Content-Length', data.length);
         res.send(data);
     });
 });
